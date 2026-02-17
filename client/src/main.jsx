52cf4fd91b619b382de7e99264333ec74d6bcfca
ik0v <ivan.kovals@hotmail.com>
2026-02-17 13:55:31 +0100

    react router added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
+import {Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Application />);
+root.render(<BrowserRouter><Application /></BrowserRouter>);
 
-function Application() {
+function FrontPage() {
     const [counter, setCounter] = useState(0);
+
     return <>
         <h2>Welcome to my application</h2>
-        <div>
-            <button onClick={() => setCounter(oldValue => oldValue + 1)}>Click me</button>
-        </div>
+            <div>
+                <button onClick={() => setCounter(oldValue => oldValue + 1)}>Click me</button>
+            </div>
         <div>You have clicked {counter} times</div>
-    </>;
+    </>
+}
+
+function Application() {
+    return <Routes>
+        <Route path={"/"} element={ <FrontPage/> }/>
+        <Route path={"*"} element={<h1>Page not found</h1>}/>
+    </Routes>;
 }
