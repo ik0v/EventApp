ebc497972fdf395264ae3f479b946276301e19ea
ik0v <ivan.kovals@hotmail.com>
2026-02-17 14:06:17 +0100

    simple get endpoint done

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 import express from "express";
 
 const app = express();
-express.use(express.static("../client/dist"));
+app.use(express.json());
+app.use(express.static("../client/dist"));
+
+export const eventsApi = new express.Router();
+
+
+eventsApi.get("/api/events", (req, res) => {
+    res.send([
+        { title: "Event 1" },
+        { title: "Event 2" },
+    ])
+});
+
+app.use(eventsApi);
+
 app.listen(3000);
